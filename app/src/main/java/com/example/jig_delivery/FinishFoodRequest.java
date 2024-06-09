7050b4074d88dd5b78753a780edfadd0271d4ddb
IgorLima <igorlimadrum@gmail.com>
2024-06-09 17:44:19 -0300

    feat: create form layout

--- app/src/main/java/com/example/jig_delivery/FinishFoodRequest.java
+++ app/src/main/java/com/example/jig_delivery/FinishFoodRequest.java
@@ -2,7 +2,10 @@ package com.example.jig_delivery;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class FinishFoodRequest extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class FinishFoodRequest extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_finish_food_request);
+
+        Spinner spPaymentMethod = (Spinner) findViewById(R.id.spPaymentMethod);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.payment_methods, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spPaymentMethod.setAdapter(adapter);
     }
 }
