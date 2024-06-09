cb8d80843f73f1855b7dff0395f06b3564dc7f0e
IgorLima <igorlimadrum@gmail.com>
2024-06-09 17:01:39 -0300

    feat: create initial activities structure

---
+++ app/src/main/java/com/example/jig_delivery/FinishFoodRequest.java
@@ -0,0 +1,14 @@
+package com.example.jig_delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FinishFoodRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_finish_food_request);
+    }
+}
---
+++ app/src/main/java/com/example/jig_delivery/RequestHistory.java
@@ -0,0 +1,14 @@
+package com.example.jig_delivery;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RequestHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_request_history);
+    }
+}
